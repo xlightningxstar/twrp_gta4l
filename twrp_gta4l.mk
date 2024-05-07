7185a74b835f0a1b7a044e24807a58459873b235
Abdalrahman Eissa <abdalrahmaneissa@gmail.com>
2024-05-07 18:57:18 +0300

    Update TWRP Tree

--- twrp_gta4l.mk
+++ twrp_gta4l.mk
@@ -2,20 +2,6 @@
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 The TWRP Open Source Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-PRODUCT_RELEASE_NAME := gta4l
 
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
@@ -31,9 +17,12 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/samsung/gta4l/device.mk)
 
 # Device identifier. This must come after all inclusions
+PRODUCT_RELEASE_NAME := gta4l
 PRODUCT_DEVICE := gta4l
 PRODUCT_NAME := twrp_gta4l
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-T505N
+PRODUCT_MODEL := SM-T507
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
